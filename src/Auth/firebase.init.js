5599a417dbf93880f2e5b3ff7747813e7f34f217
19khaled88 <69922782+19khaled88@users.noreply.github.com>
2022-04-17 02:17:23 +0600

    basic level complete

---
+++ src/Auth/firebase.init.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDSPbiixiNLE6jcs3kS5OnD7DFict-hM2Y',
+  authDomain: 'traveltoursolution-77e1f.firebaseapp.com',
+  projectId: 'traveltoursolution-77e1f',
+  storageBucket: 'traveltoursolution-77e1f.appspot.com',
+  messagingSenderId: '320000749276',
+  appId: '1:320000749276:web:650ae270896c32425b0e19',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+export default auth
---
+++ src/components/Register/Register.js
@@ -0,0 +1,122 @@
+import React, { useState } from 'react'
+
+export const Register = () => {
+  const [register, setRegister] = useState(true)
+  const registrationPageHandler = () => {
+    setRegister(!register)
+  }
+  console.log(register)
+  return (
+    <div class="container w-2/6 mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
+      {register === true ? (
+        <div>
+          <p className="text-2xl mb-4 text-teal-600">Login Page</p>
+          <div class="mb-4">
+            <label
+              class="block text-grey-darker text-sm font-bold mb-2"
+              for="username"
+            >
+              Username
+            </label>
+            <input
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+              id="username"
+              type="text"
+              placeholder="Username"
+            />
+          </div>
+          <div class="mb-6">
+            <label
+              class="block text-grey-darker text-sm font-bold mb-2"
+              for="password"
+            >
+              Password
+            </label>
+            <input
+              class="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3"
+              id="password"
+              type="password"
+              placeholder="******************"
+            />
+          </div>
+        </div>
+      ) : (
+        <div>
+          <p className="text-2xl mb-4 text-teal-600">Sign Up Page</p>
+          <div class="mb-4">
+            <label
+              class="block text-grey-darker text-sm font-bold mb-2"
+              for="username"
+            >
+              Username
+            </label>
+            <input
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+              id="username"
+              type="text"
+              placeholder="Username"
+            />
+          </div>
+          <div class="mb-6">
+            <label
+              class="block text-grey-darker text-sm font-bold mb-2"
+              for="password"
+            >
+              Password
+            </label>
+            <input
+              class="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker"
+              id="password"
+              type="password"
+              placeholder="******************"
+            />
+          </div>
+          <div class="mb-6">
+            <label
+              class="block text-grey-darker text-sm font-bold mb-2"
+              for="password"
+            >
+              Confirm Password
+            </label>
+            <input
+              class="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3"
+              id="password"
+              type="password"
+              placeholder="******************"
+            />
+          </div>
+        </div>
+      )}
+      <div className="flex flex-row justify-between">
+        <div>
+          <input
+            onClick={registrationPageHandler}
+            type="checkbox"
+            id=""
+            name=""
+            value=""
+          />
+          {register === true ? (
+            <label className="text-rose-500" for="vehicle1">
+              {' '}
+              Don't have Account?
+            </label>
+          ) : (
+            <label className="text-blue-500" for="vehicle1">
+              {' '}
+              Already have Account?
+            </label>
+          )}
+        </div>
+        <div>
+          <a
+            class="pl-2 inline-block align-baseline font-bold text-sm text-blue hover:text-blue-darker"
+            href="#"
+          >
+            Forgot Password?
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
