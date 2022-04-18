25d56d8d88e596da686fcf8f91127e22b78fbe1b
19khaled88 <kkhaled88hasan@gmail.com>
2022-04-18 16:32:07 +0600

    404

--- src/components/Notfound/Notfound.js
+++ src/components/Notfound/Notfound.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 export const Notfound = () => {
   return ( <div className="flex flex-col justify-center items-center container mb-14 pt-16 mx-auto">
-          <h4 className="">404</h4>
+          <h4 className="pt-10 text-5xl">404</h4>
+          <p>Page not found!!!</p>
          
         </div>
           );
