42a2d20efe9ec1112fa57926e4b8b4d0a2da5c0b
19khaled88 <69922782+19khaled88@users.noreply.github.com>
2022-04-18 20:35:15 +0600

    password reset

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,15 +1,19 @@
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 import React, { useRef, useState } from 'react'
+import { Modal, Button } from 'react-bootstrap'
 import {
   useAuthState,
   useCreateUserWithEmailAndPassword,
   useSignInWithEmailAndPassword,
+  useSendPasswordResetEmail,
 } from 'react-firebase-hooks/auth'
 import { useNavigate, useLocation } from 'react-router-dom'
 import auth from '../../Auth/firebase.init'
 
 export const Register = () => {
   const [register, setRegister] = useState(true)
+  const [email, setEmail] = useState('')
+  const [showModal, setShowModal] = useState(false)
   const [loading] = useAuthState(auth)
   const [
     createUserWithEmailAndPassword,
@@ -23,6 +27,9 @@ export const Register = () => {
     loading2,
     error2,
   ] = useSignInWithEmailAndPassword(auth)
+  const [sendPasswordResetEmail, sending, error3] = useSendPasswordResetEmail(
+    auth,
+  )
   const [user, setUser] = useState('')
   const [error, setError] = useState('')
   const navigate = useNavigate()
@@ -106,7 +113,12 @@ export const Register = () => {
     }
     createUserWithEmailAndPassword(email, password)
   }
-
+  const resetEmailHandler = async () => {
+    await sendPasswordResetEmail(email)
+    console.log(email)
+    setShowModal(false)
+    alert('Sent Email')
+  }
   return (
     <div class="container absolute inset-0 top-16 w-2/6 mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
       {register === true ? (
@@ -285,11 +297,67 @@ export const Register = () => {
           )}
         </div>
         <div>
-          <button class="pl-2 inline-block align-baseline font-bold text-sm text-blue hover:text-blue-darker">
+          <button
+            onClick={() => setShowModal(true)}
+            class="pl-2 inline-block align-baseline font-bold text-sm text-blue hover:text-blue-darker"
+          >
             Forgot Password?
           </button>
         </div>
       </div>
+
+      {showModal ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">Reset Password</h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto border-solid  ">
+                  <input
+                    className="border-2 border-indigo-600"
+                    placeholder="Email Address"
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Close
+                  </button>
+                  <button
+                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={resetEmailHandler}
+                    // onClick={() => setShowModal(false)}
+                  >
+                    Reset
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
     </div>
   )
 }
