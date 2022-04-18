a35ed73df867e31411b799e89b73980eb4a3ab2c
19khaled88 <kkhaled88hasan@gmail.com>
2022-04-18 16:29:25 +0600

    about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,5 +3,8 @@ import React from 'react'
 export const About = () => {
   return <div className="flex flex-col justify-center items-center container mb-14 pt-16 mx-auto">
             <h4 className="text-2xl">About Page:</h4>
+            <p><span className="text-green-500 pr-5">Name:</span> Mohammad Khaeld Ahasan</p>
+            <img className="h-80" src="/images/professionalImage.jpg" alt="" />
+            <p><span className="text-orange-500 pr-5">Goal:</span>My goal is to be a successful developer so that i become able to serve as many professional services to endpoint customer as possible. I want to follow successful developer who works like poineer in the ground of coding. Moreover, i practice daily and try to learn more coding methods as i can. I rely on following documentation or respecting library or framework so that i know inners of that library best.</p>
           </div>
 }
